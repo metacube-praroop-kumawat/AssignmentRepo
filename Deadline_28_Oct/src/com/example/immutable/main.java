dcbada084f5e06c5959ec5bb99c18d93b101d1ba
Praroop Kumawat <praroop.kumawat@metacube.com>
2024-10-28 23:32:06 +0530

    added poly class file

--- Deadline_28_Oct/src/com/example/immutable/main.java
+++ Deadline_28_Oct/src/com/example/immutable/main.java
@@ -3,11 +3,12 @@ package com.example.immutable;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//IntSet class functions called
 		IntSet IntegerArrayObject1 = new IntSet(new Integer[]{1,3,6,7});
 		IntSet integerArrayObject2 = new IntSet(new Integer[]{2,3,6});
-//		IntegerArrayObject1.union(integerArrayObject2);
-		//System.out.println(IntegerArrayObject1.getComplement());
+		IntegerArrayObject1.union(integerArrayObject2);
+		System.out.println(IntegerArrayObject1.getComplement());
+		//Poly class function called
 		Integer arr[][] = {{2,1}, {1,3}, {0,1}};
 		Poly polynomial = new Poly(arr);
 		Integer arr1[][] = {{4,1},{3,1},{2,1},{1,1},{0,1},{5,3}};
