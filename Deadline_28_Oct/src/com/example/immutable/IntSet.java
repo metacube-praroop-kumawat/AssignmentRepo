97919c8e507bf916abc8808a91950fcbe5086e22
Praroop Kumawat <praroop.kumawat@metacube.com>
2024-10-29 12:47:39 +0530

    added comments

--- Deadline_28_Oct/src/com/example/immutable/IntSet.java
+++ Deadline_28_Oct/src/com/example/immutable/IntSet.java
@@ -1,7 +1,17 @@
+/*******************************************************************************************************
+* @classname: IntSet
+* @author: Praroop
+* Modification Log:
+********************************************************************************************************
+* Praroop Kumawat        2024/10/29         Making functions to operate on Sets
+********************************************************************************************************/
+
+
 package com.example.immutable;
 
 public final class IntSet {
-	private final Integer[] set; //array representing integers in the range 1-1000
+	//array representing integers in the range 1-1000
+	private final Integer[] set; 
 	
 	//constructor
 	public IntSet(Integer[] elements) {
@@ -16,51 +26,69 @@ public final class IntSet {
 			}
 		}
 	}
-	
-	//check if an integer x is a member of the set
-	public boolean isMember(int x ) {
-		for( Integer value : set ) {
-			if ( value == x ) {
+	/**
+	 * check if parameter x is a part of set or not
+	 * @param x to value in Set which we are looking for
+	 * @return boolean true if x is present 
+	 */
+	public boolean isMember(int x) {
+		//iterating on set
+		for (Integer value : set) {
+			if (value == x) {
 				return true;
 			}
 		}
 		return false;
 	}
 	
-	//return the size of the set
+	/**
+	 * gives the size of set
+	 * @return int size of set
+	 */
 	public int size() {
 		return set.length;
 	}
 	
-	//check whether s is a subset or not
-	public boolean isSubSet ( IntSet array ) {
+	/**
+	 * check whether IntSet array is a subset or not
+	 * @param array the subset
+	 * @return boolean true if yes subset
+	 */
+	public boolean isSubSet (IntSet array) {
 		int matchedElements = 0;
-		for ( Integer valueInSubSet : array.set ) {
-			for ( int i = 0; i < set.length; i++ ) {
-				if ( valueInSubSet == set[i] ) {
+		//checking the values of subset are present in set or not
+		for (Integer valueInSubSet : array.set) {
+			for (int i = 0; i < set.length; i++) {
+				if (valueInSubSet == set[i]) {
 					matchedElements++;
 				}
 			}
 		}
-		if ( matchedElements == array.set.length) {
+		//all the elements of subset is present in set
+		if (matchedElements == array.set.length) {
 			return true;
 		}
 		return false;
 	}
 	
-	//get component set (1-1000 elements not present in the current set)
+	/**
+	 *get component set (1-1000 elements not present in the current set)
+	 * @return Integer type array of complement 
+	 */
 	public Integer[] getComplement() {
+		//universal array to see what elements are not present in set
 		Integer universalSet[] = new Integer[1000];
-		for(int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 1000; i++) {
 			universalSet[i] = 0;
 		}
-		for ( Integer elements : set ) {
+		for (Integer elements : set) {
 			universalSet[elements-1] = elements;
 		}
 		Integer complement[] = new Integer[1000-set.length];
 		int j = 0; 
-		for ( int i = 0; i < universalSet.length; i++ ) {
-			if ( universalSet[i] == 0 ) {
+		//if universal array have 0 value then the element is not present in set
+		for (int i = 0; i < universalSet.length; i++) {
+			if (universalSet[i] == 0) {
 				complement[j] = i+1;
 				j++;
 			}
@@ -69,28 +97,33 @@ public final class IntSet {
 		return complement;
 	}
 	
-	//get union of two sets
-	public Integer[] union ( IntSet intSet ) {
+	/**
+	 * get union of two sets
+	 * @param intSet IntSet object in which array is present
+	 * @return Integer array resulted by union 
+	 */
+	public Integer[] union(IntSet intSet) {
+		//universal array to see what elements are present in set
 		boolean universalSet[] = new boolean[1000];
-		/*for ( int i = 0, j = 0; i < set.length, j < intSet.set.length; i++, j++ ) {
-			
-		}*/
-		for ( int i = 0; i < set.length; i++) {
+		//taking input for set1
+		for (int i = 0; i < set.length; i++) {
 			universalSet[set[i]-1] = true;
 		}
-		for( int i = 0; i < intSet.set.length; i++ ) {
+		//taking input for set2
+		for (int i = 0; i < intSet.set.length; i++) {
 			universalSet[intSet.set[i]-1] = true;
 		}
 		int ansSize = 0;
 		int index = 0;
-		for( int i = 0; i < universalSet.length; i++ ) {
-			if(universalSet[i] == true) {
+		for (int i = 0; i < universalSet.length; i++) {
+			if (universalSet[i] == true) {
 				ansSize++;
 			}
 		}
+		//inserting union elements from universal array to union array
 		Integer union[] = new Integer[ansSize];
-		for( int i = 0; i < universalSet.length; i++ ) {
-			if(universalSet[i] == true) {
+		for (int i = 0; i < universalSet.length; i++) {
+			if (universalSet[i] == true) {
 				union[index] = i+1;
 				index ++;
 			}
@@ -99,9 +132,14 @@ public final class IntSet {
 		return union;		
 	}
 	
+	/**
+	 * printing the elements of set
+	 * @param set of which we want to print elements
+	 */
 	public void printSet(Integer set[]) {
-		for (int i = 0; i < set.length; i++ ) {
+		for (int i = 0; i < set.length-1; i++) {
 			System.out.print(set[i]+", ");
 		}
+		System.out.print(set[set.length-1]);
 	}
 }
--- Deadline_28_Oct/src/com/example/immutable/main.java
+++ Deadline_28_Oct/src/com/example/immutable/main.java
@@ -4,17 +4,21 @@ public class main {
 
 	public static void main(String[] args) {
 		//IntSet class functions called
-//		IntSet IntegerArrayObject1 = new IntSet(new Integer[]{1,3,6,7});
-//		IntSet integerArrayObject2 = new IntSet(new Integer[]{2,3,6});
-//		IntegerArrayObject1.union(integerArrayObject2);
-//		System.out.println(IntegerArrayObject1.getComplement());
+		IntSet integerArrayObject1 = new IntSet(new Integer[]{1,3,6,7});
+		IntSet integerArrayObject2 = new IntSet(new Integer[]{2,3,6});
+		System.out.print("The union of two sets are:" + '\n');
+		integerArrayObject1.union(integerArrayObject2);
+		System.out.println();
+		System.out.println();
+		System.out.println();
 		//Poly class function called
 		Integer arr[][] = {{2,1}, {1,3}, {0,1}};
 		Poly polynomial = new Poly(arr);
 		Integer arr1[][] = {{4,1},{3,1},{2,1},{1,1},{0,1},{5,3}};
 		Poly poly2 = new Poly(arr1);
 		Poly poly3 = poly2.addPolynomial(polynomial);
-		System.out.println(poly3.printPolynomial());
+		System.out.println("The resultant polynomial from the addition of two polynomial " + '\n' + polynomial.printPolynomial() +
+				" and " + '\n' + poly2.printPolynomial() + " is :- " + '\n' + poly3.printPolynomial());
 	}
 
 }
